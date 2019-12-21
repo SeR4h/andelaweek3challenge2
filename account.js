0626265363ca462a52008a2ef9ad6bb0c9e23350
SeR4h <40196738+SeR4h@users.noreply.github.com>
2019-12-21 13:48:27 +0300

    Updated bank account

--- account.js
+++ account.js
@@ -1,104 +1,59 @@
-var readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 class BankAccount {
-  constructor(name, accountNumber, phoneNumber, balance, accountStatus) {
+  constructor(name, accountNumber, balance, accountStatus) {
     this.balance = balance;
     this.name = name;
     this.accountNumber = accountNumber;
-    this.phoneNumber = phoneNumber;
     this.accountStatus = accountStatus;
   }
-
-  open() {
-    var account = new BankAccount()
-    rl.question('Enter your name  :', function (name) {
-      rl.question('Enter accountNumber: ', function (accountNumber) {
-        rl.question('Enter phone number: ', function (phoneNumber) {
-          account.name = name
-          account.accountNumber = accountNumber
-          account.phoneNumber = phoneNumber
-          console.log(account)
-          console.log('account opened')
-        });
-      });
-    });
+  open(name, balance) {
+    this.name = name;
+    this.balance = balance;
+    this.accountStatus = 'active';
+    return this.accountNumber;
 
   }
-  deposit(depositAmount, accountNumber) {
-    if (this.accountNumber === accountNumber) {
+
+  deposit(depositAmount, accountNumber, accountStatus) {
+    if (this.accountNumber === accountNumber || this.accountStatus === accountStatus) {
       console.log('You have deposited' + depositAmount)
-      var newbalance = this.balance + depositAmount
-      return newbalance;
-    }
-    else {
-      return 'account doesnt exist'
-    }
-  }
-  getBalance(accountNumber) {
-    if (this.accountNumber === accountNumber) {
+      var newBalance = this.balance + depositAmount
+      this.balance = newBalance;
       return this.balance;
     }
     else {
-      return 'account doesnt exist'
+      throw new Error('account doesnt exist')
     }
   }
 
-  withdraw(withdrawAmount, accountNumber) {
-    if (this.accountNumber === accountNumber) {
-
-      var balance = this.balance - withdrawAmount;
-
-      console.log('You have withdrawn' + withdrawAmount)
-      console.log('Account balance is' + balance)
-      return balance;
+  getBalance(accountNumber, accountStatus) {
+    if (this.accountNumber === accountNumber || this.accountStatus === accountStatus) {
+      return this.balance;
     }
     else {
-      return 'account doesnt exist'
-
+      throw new Error('account doesnt exist or account is closed')
     }
+  }
 
+  withdraw(withdrawAmount, accountNumber, accountStatus) {
+    if (this.accountNumber === accountNumber || this.accountStatus === accountStatus) {
+      this.balance -= withdrawAmount;
+      console.log('You have withdrawn' + withdrawAmount)
+      console.log('Account balance is' + this.balance)
+      return this.balance;
+    }
+    else {
+      throw new Error('account doesnt exist or account closed')
+    }
   }
 
   close(accountNumber) {
-    if (this.accountNumber === accountNumber || this.accountStatus === 'open') {
+    if (this.accountNumber === accountNumber || this.accountStatus === 'active') {
       this.accountStatus = 'closed'
       return 'Account closed successfully';
     }
     else {
-      return 'account doesnt exist'
+      throw new Error('account doesnt exist')
     }
   }
 }
-module.exports = new BankAccount('sarah', 'SB1034', '0704962749', 500000, 'open');
-var o = new BankAccount()
-console.log('Bank options: \n open account , deposit , check balance , withdraw , close account ')
-rl.question('Type option: ', function (option) {
-  if (option === 'open account') {
-        o.open()
-  }
-  else if (option === 'deposit') {
-    rl.question('Enter accountNumber  :', function (accountNumber) {
-      rl.question('Enter amount to deposit : ', function (depositamount) {
-       o.deposit(depositamount, accountNumber)
-      });
-    });
-    
-  }
-  else if (option === 'check balance') {
-    o.getBalance()
-  }
-  else if (option === 'withdraw') {
-    o.withdraw()
-  }
-  else if (option === 'close account') {
-    o.close()
-  }
-  else {
-    console.log('Invalid input')
-    rl.close();
-  }
-});
-
+module.exports = new BankAccount('sarah', 'SB1034', 500000, 'active');
--- account.test.js
+++ account.test.js
@@ -1,27 +1,30 @@
 const BankAccount = require('./account');
 
+test('returns new accout number ', () => {
+    expect(BankAccount.open('sarah', 0)).toBe('SB1034');
+});
 test('returns account balance after deposit ', () => {
-    expect(BankAccount.deposit(10000, 'SB1034')).toBe(510000);
+    expect(BankAccount.deposit(500000, 'SB1034', 'active')).toBe(500000);
 });
 test('returns null deposited if user account number doesnt exist', () => {
-    expect(BankAccount.deposit(500000, 'SB134')).toBe('account doesnt exist');
+    expect(() => { BankAccount.deposit(500000, 'SB1378') }).toThrowError('account doesnt exist');
 });
 test('returns balance', () => {
     expect(BankAccount.getBalance('SB1034')).toBe(500000);
 });
 test('exits if user account number doesnt exist', () => {
-    expect(BankAccount.getBalance('SB134')).toBe('account doesnt exist');
+    expect(() => { BankAccount.getBalance('SB1356', 'closed') }).toThrowError('account doesnt exist or account is closed');
 });
 
-test('returns amount withdrawn', () => {
+test('returns balance after withdrawing', () => {
     expect(BankAccount.withdraw(200000, 'SB1034')).toBe(300000);
 });
-test('exits if user account number doesnt exist', () => {
-    expect(BankAccount.withdraw(200000, 'SB134')).toBe('account doesnt exist');
+test('exits if user account is closed', () => {
+    expect(() => { BankAccount.withdraw(200000, 'SB135', 'closed') }).toThrowError('account doesnt exist or account closed');
 });
 test('returns account closed', () => {
     expect(BankAccount.close('SB1034')).toBe('Account closed successfully');
 });
 test('exits if user account number doesnt exist', () => {
-    expect(BankAccount.close('SB134')).toBe('account doesnt exist');
+    expect(() => { BankAccount.close('SB1345') }).toThrowError('account doesnt exist');
 });
